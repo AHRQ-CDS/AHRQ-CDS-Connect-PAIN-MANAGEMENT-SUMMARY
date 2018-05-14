636507a497cc3c27e6598f66c51bd0c4ff22d40f
Chris Moesel <cmoesel@mitre.org>
2018-05-14 17:13:23 -0400

    upload-test-patients uploads ALL patients in test_patients flder

--- src/utils/uploadTestPatients.js
+++ src/utils/uploadTestPatients.js
@@ -1,5 +1,6 @@
+const fs = require('fs');
+const path = require('path');
 const mkFhir = require('fhir.js');
-const fullerJackson = require('./test_patients/fuller_jackson.json');
 const baseUrl = 'http://launch.smarthealthit.org/v/r2/fhir';
 
 function convertToTx(bundle) {
@@ -15,15 +16,51 @@ function convertToTx(bundle) {
   return tx;
 }
 
-const tx = convertToTx(fullerJackson);
+function stringResult(response) {
+  const name = response.config.bundle.id;
+  const status = response.status;
+  const statusMap = {};
+  if (response.data && response.data.entry && response.data.entry) {
+    response.data.entry.forEach(entry => {
+      const entryStatus = entry.response && entry.response.status ? entry.response.status : 'unknown';
+      if (!statusMap[entryStatus]) {
+        statusMap[entryStatus] = 1;
+      } else {
+        statusMap[entryStatus] = statusMap[entryStatus] + 1;
+      }
+    });
+  }
+  return name.padEnd(25) + '     ' + `${status}`.padEnd(9) + '     ' + JSON.stringify(statusMap);
+}
 
 const client = mkFhir({ baseUrl });
 
-client.transaction({ bundle: tx }).then(
-  (entry) => {
-    console.log('SUCCESS', JSON.stringify(entry, null, 2));
-  },
-  (error) => {
-    console.error('ERROR', JSON.stringify(error, null, 2));
+console.log('-'.padEnd(80, '-'));
+console.log('bundle id                     tx status     entry statuses')
+console.log('-'.padEnd(80, '-'));
+
+const testPatientsPath = path.join(__dirname, 'test_patients');
+for (const fileName of fs.readdirSync(testPatientsPath)) {
+  const file = path.join(testPatientsPath, fileName);
+  if (!file.endsWith('.json')) {
+    console.log('Skipping non-JSON file:', file);
+    continue;
   }
-);
+
+  const json = JSON.parse(fs.readFileSync(file, 'utf8'));
+  if (json.resourceType !== 'Bundle') {
+    console.error('Test patients must be a valid FHIR Bundle.  Not a Bundle:', file);
+    continue;
+  }
+
+  client.transaction({ bundle: convertToTx(json) }).then(
+    (success) => {
+      console.log(stringResult(success));
+    },
+    (error) => {
+      console.error(stringResult(error));
+    }
+  );
+}
+
+
