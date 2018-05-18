58f15bc10d8a1baa1cf7cbfa955032eb9a7be23f
Chris Moesel <cmoesel@mitre.org>
2018-05-17 21:45:30 -0400

    Disable service-worker
    The service-worker makes the app a progressive offline-first application.  This is not necessary for our use case, and the agressive caching may be a liability.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import Root from './containers/Root';
-import registerServiceWorker from './utils/registerServiceWorker';
+// Disabling the service-worker, as it has limited value in this environment
+// See: https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/template/README.md#making-a-progressive-web-app
+// import registerServiceWorker from './utils/registerServiceWorker';
 import '../node_modules/font-awesome/css/font-awesome.css';
 import './styles/App.css';
 
@@ -15,5 +17,4 @@ render(
   </Router>,
   document.getElementById('root')
 );
-
-registerServiceWorker();
+// registerServiceWorker();
