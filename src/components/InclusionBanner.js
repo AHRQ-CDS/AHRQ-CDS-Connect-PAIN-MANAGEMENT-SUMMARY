7abedaa4a83a8b2d0c0f49629476ffe93cd0b40a
Noranda Brown <nebrown@mitre.org>
2019-11-22 09:10:21 -0500

    Moved up inclusion banner

--- src/components/InclusionBanner.js
+++ src/components/InclusionBanner.js
@@ -19,6 +19,10 @@ export default class InclusionBanner extends Component {
       <div className="inclusion-banner banner" style={{display: this.state.displayed ? 'block' : 'none'}}>
         {this.props.dismissible && <FontAwesome className="close-button" name="close" onClick={this.handleClose} />}
 
+        <div className="inclusion-banner__tag banner warning-inside">
+          This patient does not meet the applicable criteria.
+        </div>
+
         <div className="inclusion-banner__description">
           <strong><FontAwesome name="exclamation-circle" /> WARNING:</strong> This summary applies to patients
           18 years or older who meet at least one of the following criteria:
@@ -28,10 +32,6 @@ export default class InclusionBanner extends Component {
             <li>Has an active opioid medication in the last 180 days</li>
             <li>Has an active adjuvant analgesic medication in the last 180 days</li>
           </ul>
-        </div>
-
-        <div className="inclusion-banner__tag banner warning-inside">
-          This patient does not meet the applicable criteria.
         </div>
       </div>
     );
