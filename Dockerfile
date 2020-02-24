c7ff0eed0fa77d35078a1fb7cfd2904177598b97
Ivan Cvitkovic <ivanc@uw.edu>
2020-02-23 19:08:12 -0800

    Backend Authentication (#6)
    * Load SoF context from confidential back-end
    
    * WIP: add production Dockerfile
    
    * Load launch context from configured backend
    
    * Use common env file
    
    * Make port/dockerfile configurable
    
    * cleanup
    
    * Update default config

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,6 @@ COPY . .
 
 RUN yarn --modules-folder=/opt/node_modules
 
-
 # yarn ignores its own config (--modules-folder); symlink node_modules where it expects
 # https://github.com/yarnpkg/yarn/issues/3900
 CMD ln -fs /opt/node_modules && yarn start
--- src/launch.js
+++ src/launch.js
@@ -1,6 +1,15 @@
 import FHIR from 'fhirclient';
 
-fetch(`${process.env.PUBLIC_URL}/launch-context.json`)
+// retrieve launch context from backend, if configured
+let context_url = `${process.env.PUBLIC_URL}/launch-context.json`;
+if (process.env.REACT_APP_CONF_API_URL){
+  context_url = `${process.env.REACT_APP_CONF_API_URL}/auth/auth-info`;
+}
+
+fetch(context_url, {
+  // include cookies in request
+  credentials: 'include'
+})
   .then((response)      => response.json())
   .then((launchContext) => FHIR.oauth2.authorize(launchContext))
   .catch((error)        => console.error(error));
