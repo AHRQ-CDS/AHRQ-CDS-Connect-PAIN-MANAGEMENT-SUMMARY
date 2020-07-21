c782e63de061787541f5c5efd4017c609ca4a9af
Jason Evans <jason@databaseconsultinggroup.com>
2020-07-21 10:59:13 -0600

    Remove unnecessary "decode" of patient ID from launch.js

--- src/launch.js
+++ src/launch.js
@@ -1,14 +1,7 @@
 import FHIR from 'fhirclient';
-
-let params = new URLSearchParams(document.location.search.substring(1));
-let encodedLaunch = params.get('launch');
-let decodedLaunch = atob(encodedLaunch);
-let launchContextIn = JSON.parse(decodedLaunch);
-
 fetch(`${process.env.PUBLIC_URL}/launch-context.json`)
   .then((response)      => response.json())
   .then((launchContext) => {
-      launchContext.patientId = launchContextIn.b;
       return FHIR.oauth2.authorize(launchContext);
   })
   .catch((error)        => console.error(error));
